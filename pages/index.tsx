fffef7273abce18ba062d51558f73e0e4c900e3e
maggiezetaa <16521475@std.stei.itb.ac.id>
2024-05-04 21:43:19 +0700

    add: right-sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,92 +5,173 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen">
-      <aside className="w-1/4 bg-gray-800 text-white p-4">
+    <div className="app-container" style={{ display: "flex", justifyContent: "space-between", alignItems: "stretch" }}>
+      <div className="sidebar left-sidebar w-[90%] bg-gray-800 text-white p-4 " style={{ flex: 1, margin: 10 }}>
         <h1 className="text-lg font-bold">WebGL 3D Engine</h1>
 
-        <div className="mt-4">
-          <h2 className="font-semibold">Projection</h2>
-          <select id="projection-type" name="projection-type"
-            className="block appearance-none w-30 bg-white border border-gray-200 text-gray-700 py-2 px-3 pr-8 rounded leading-tight cursor-pointer focus:outline focus:border-black">
-            <option>Orthographic</option>
-            <option>Perspective</option>
-            <option>Oblique</option>
-          </select>
-        </div>
+        <div className="flex justify-between mt-4">
+          <div className="mt-4">
+            <h2 className="font-semibold">Projection</h2>
+            <select id="projection-type" name="projection-type"
+              className="block appearance-none w-30 bg-white border border-gray-200 text-gray-700 py-2 px-3 pr-8 rounded leading-tight cursor-pointer focus:outline focus:border-black">
+              <option>Orthographic</option>
+              <option>Perspective</option>
+              <option>Oblique</option>
+            </select>
+          </div>
 
-        <div className="mt-4">
-          <h2 className="font-semibold">Shape</h2>
-          <select id="projection-type" name="projection-type"
-            className="block appearance-none w-30 bg-white border border-gray-200 text-gray-700 py-2 px-3 pr-8 rounded leading-tight cursor-pointer focus:outline focus:border-black">
-            <option>Shape1</option>
-            <option>Shape2</option>
-            <option>Shape3</option>
-          </select>
+          <div className="mt-4">
+            <h2 className="font-semibold">Shape</h2>
+            <select id="projection-type" name="projection-type"
+              className="block appearance-none w-30 bg-white border border-gray-200 text-gray-700 py-2 px-3 pr-8 rounded leading-tight cursor-pointer focus:outline focus:border-black">
+              <option>Shape1</option>
+              <option>Shape2</option>
+              <option>Shape3</option>
+              <option>Shape4</option>
+            </select>
+          </div>
         </div>
 
         <div className="mt-4">
           <h2 className="font-semibold">Transformation</h2>
-          {/* Translate Sliders */}
-          {['X', 'Y', 'Z'].map(axis => (
-            <div key={`translate-${axis}`} className="flex items-center justify-between my-2">
-              <label>Translate {axis}</label>
-              <input type="range" min="-1000" max="1000" defaultValue="0" className="w-2/3" />
+          <div className="grid grid-cols-3 gap-4">
+            <div>
+              <label>Rotate</label>
+              <div>
+                <label>X</label>
+                <input
+                  type="range"
+                  min="-1000" max="1000" defaultValue="0"
+                  className="w-full mt-1"
+                />
+                <label>Y</label>
+                <input
+                  type="range"
+                  min="-1000" max="1000" defaultValue="0"
+                  className="w-full mt-1"
+                />
+                <label>Z</label>
+                <input
+                  type="range"
+                  min="-1000" max="1000" defaultValue="0"
+                  className="w-full mt-1"
+                />
+              </div>
             </div>
-          ))}
-          {/* Angle Sliders */}
-          {['X', 'Y', 'Z'].map(axis => (
-            <div key={`angle-${axis}`} className="flex items-center justify-between my-2">
-              <label>Angle {axis}</label>
-              <input type="range" min="0" max="360" defaultValue="0" className="w-2/3" />
+
+            <div>
+              <label>Translate</label>
+              <div>
+                <label>X</label>
+                <input
+                  type="range"
+                  min="0" max="360" defaultValue="0"
+                  className="w-full mt-1"
+                />
+                <label>Y</label>
+                <input
+                  type="range"
+                  min="0" max="360" defaultValue="0"
+                  className="w-full mt-1"
+                />
+                <label>Z</label>
+                <input
+                  type="range"
+                  min="0" max="360" defaultValue="0"
+                  className="w-full mt-1"
+                />
+              </div>
             </div>
-          ))}
-          {/* Scale Sliders */}
-          {['X', 'Y', 'Z'].map(axis => (
-            <div key={`scale-${axis}`} className="flex items-center justify-between my-2">
-              <label>Scale {axis}</label>
-              <input type="range" min="0" max="10" defaultValue="1" step="0.1" className="w-2/3" />
+
+            <div>
+              <label>Scale</label>
+              <div>
+                <label>X</label>
+                <input
+                  type="range"
+                  min="0" max="10" defaultValue="0"
+                  className="w-full mt-1"
+                />
+                <label>Y</label>
+                <input
+                  type="range"
+                  min="0" max="10" defaultValue="0"
+                  className="w-full mt-1"
+                />
+                <label>Z</label>
+                <input
+                  type="range"
+                  min="0" max="10" defaultValue="0"
+                  className="w-full mt-1"
+                />
+              </div>
             </div>
-          ))}
+          </div>
         </div>
 
         <div className="mt-4">
           <h2 className="font-semibold">Camera Controls</h2>
           <div className="flex items-center justify-between my-2">
             <label>Angle</label>
-            <input type="range" min="0" max="360" defaultValue="0" step="0.1" className="w-2/3" />
+            <input type="range" min="0" max="360" defaultValue="0" className="w-2/3" />
           </div>
           <div className="flex items-center justify-between my-2">
             <label>Radius</label>
-            <input type="range" min="0" max="1000" defaultValue="0" step="0.1" className="w-2/3" />
+            <input type="range" min="0" max="1000" defaultValue="0" className="w-2/3" />
           </div>
         </div>
 
-        <div className="flex justify-between mt-4">
+        <div className="flex mt-4">
           <div className="mt-4">
             <h2 className="font-semibold">Animation</h2>
-            <button className="mt-2 px-4 py-2 bg-green-500 rounded hover:bg-green-700">On</button>
-            <button className="mt-2 px-4 py-2 bg-red-500 rounded hover:bg-red-700">Off</button>
+            <input type="radio" name="animation" id="play" /> <label htmlFor="play">Play</label><br />
+            <input type="radio" name="animation" id="pause" /> <label htmlFor="pause">Pause</label><br />
+            <input type="radio" name="animation" id="reverse" /> <label htmlFor="reverse">Reverse</label><br />
+            <input type="radio" name="animation" id="autoReplay" /> <label htmlFor="autoReplay">Auto-replay</label><br />
           </div>
 
-          <div className="mt-4">
-            <h2 className="font-semibold">Shading</h2>
-            <button className="mt-2 px-4 py-2 bg-green-500 rounded hover:bg-green-700">On</button>
-            <button className="mt-2 px-4 py-2 bg-red-500 rounded hover:bg-red-700">Off</button>
+          <div className="ml-20 mt-4">
+            <h2 className="font-semibold">Frame</h2>
+            <input type="radio" name="frame" id="next" /> <label htmlFor="next">Next</label><br />
+            <input type="radio" name="frame" id="previous" /> <label htmlFor="previous">Previous</label><br />
+            <input type="radio" name="frame" id="first" /> <label htmlFor="first">First</label><br />
+            <input type="radio" name="frame" id="last" /> <label htmlFor="last">Last</label><br />
           </div>
         </div>
 
         <div className="mt-4">
-          <h2 className="font-semibold">Load Model</h2>
-          <input type="file" className="mt-2 text-sm text-gray-800" />
+          <h2 className="font-semibold">Option</h2>
+          <div className="flex flex-col space-y-2">
+            <button className="px-2 py-1 bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">Load</button>
+            <button className="px-2 py-1 bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">Save</button>
+            <button className="px-2 py-1 bg-red-500 rounded shadow hover:bg-red-700">Reset</button>
+          </div>
         </div>
+      </div>
 
-      </aside>
+      <div className="canvas flex-grow p-4 " style={{ flex: 3, margin: 10, backgroundColor: "#fff" }}>
+        <canvas id="webgl-canvas" className=" outline w-[95%] h-[70%]"></canvas>
+      </div>
 
-      <main className="flex-grow p-4">
-        <canvas id="webgl-canvas" className="bg-white outline w-full h-full"></canvas>
-      </main>
-
+      <div className="sidebar right-sidebar w-1/4 bg-gray-800 text-white p-4 overflow-y-auto" style={{ flex: 1, margin: 10 }}>
+        <div className="mt-4">
+          <h2 className="font-semibold">Component Tree</h2>
+          <div className="flex flex-col space-y-2">
+            <button className="bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">RBody</button>
+            <button className="bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">PBody</button>
+            <button className="bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">RHead</button>
+            <button className="bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">PHead</button>
+            <button className="bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">RArmL</button>
+            <button className="bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">PArmL</button>
+            <button className="bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">RArmR</button>
+            <button className="bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">PArmR</button>
+            <button className="bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">RLegL</button>
+            <button className="bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">PLegL</button>
+            <button className="bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">RLegR</button>
+            <button className="bg-white text-gray-800 border border-gray-300 rounded shadow hover:bg-gray-100">PLegR</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
