69df60c084b4e8bad8cbb10ee946063a014b4363
mikeleo03 <leonmichael463@gmail.com>
2024-05-15 15:18:35 +0700

    [Feat] MaterialUtils

---
+++ lib/data/components/materials/MaterialUtil.ts
@@ -0,0 +1,21 @@
+import { MaterialType } from "../../types/gltftypes";
+import { ShaderMaterial } from "./ShaderMaterial";
+import { BasicMaterial } from "./BasicMaterial";
+import { PhongMaterial } from "./PhongMaterial";
+
+export class MaterialUtil {
+    static fromRaw(raw: MaterialType): ShaderMaterial { 
+        if (raw.type === "Basic Material") {
+            const obj = new BasicMaterial(raw);
+            ShaderMaterial.fromRaw(raw, obj);
+            return obj;
+        }
+        else if (raw.type === "Phong Material") {
+            const obj = new PhongMaterial(raw);
+            ShaderMaterial.fromRaw(raw, obj);
+            return obj;
+        }
+
+        throw new Error(`Unknown material type}`);
+    }
+}
