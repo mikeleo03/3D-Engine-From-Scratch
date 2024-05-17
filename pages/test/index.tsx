7eaaa86eca77e32d72a024a14bfe8592f7f22bf9
mikeleo03 <leonmichael463@gmail.com>
2024-05-17 16:46:54 +0700

    [Feat] Add test

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -14,42 +14,57 @@ export default function Page() {
       // IMPORTANT: use dynamic import to avoid loading the entire library at server side
       const { GLContainer } = await import('@/lib/cores/GLContainer');
       const { OrthographicCamera } = await import('@/lib/data/components/cameras/OrthographicCamera');
+      const { PerspectiveCamera } = await import('@/lib/data/components/cameras/PerspectiveCamera');
+      const { ObliqueCamera } = await import('@/lib/data/components/cameras/ObliqueCamera');
       const { GLRenderer } = await import('@/lib/rendering/GLRenderer');
       const { LeonModel } = await import('@/lib/data/models/LeonModel');
       const { RenderManager} = await import('@/lib/rendering/RenderManager');
+      const { SceneNode } = await import('@/lib/data/SceneNode');
 
       const glContainer = new GLContainer(canvas);
 
-      // const camera = new PerspectiveCamera(
-      //   canvas.width / canvas.height,
-      //   60,
-      //   0.01,
-      //   1000
-      // );
-
-      const camera = new OrthographicCamera(
-        1,
-        -1,
-        -1,
-        1,
+      const orthographicCamera = new OrthographicCamera(
+        canvas.height/2,
+        -canvas.height/2,
+        -canvas.width/2,
+        canvas.width/2,
         0.01,
         1000
       );
 
-      // const camera = new ObliqueCamera(
-      //   1,
-      //   -1,
-      //   -1,
-      //   1,
-      //   0.01,
-      //   100,
-      //   20
-      // );
+      const perspectiveCamera = new PerspectiveCamera(
+          canvas.width / canvas.height,
+          20,
+          0.01,
+          9999
+      );
 
-      const model = new LeonModel();
+      const obliqueCamera = new ObliqueCamera(
+          1,
+          -1,
+          -1,
+          1,
+          0.01,
+          100,
+          20
+      );
 
-      console.log(model.scene.nodes)
-      model.download();
+      const cameraNodes = [
+          new SceneNode(undefined, undefined, undefined, undefined, undefined, orthographicCamera),
+          new SceneNode(undefined, undefined, undefined, undefined, undefined, perspectiveCamera),
+          new SceneNode(undefined, undefined, undefined, undefined, undefined, obliqueCamera)
+      ]
+
+      const leonModel = new LeonModel();
+      
+      const glRenderer = new GLRenderer(glContainer);
+
+      const scene = leonModel.scene;
+
+      scene.addNode(cameraNodes[1]);
+
+      glRenderer.render(scene);
+      // leonModel.download();
     };
 
     initializeGL();
