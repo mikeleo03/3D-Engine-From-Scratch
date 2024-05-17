5cd8e559539c39174f754e57d1155f28411d5b0c
Enliven26 <16521443@mahasiswa.itb.ac.id>
2024-05-18 06:41:39 +0700

    refactor: test page

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,64 +1,7 @@
+import dynamic from 'next/dynamic';
 
-import { useEffect, useRef } from "react";
+const TestPage = dynamic(() => import('./testpage'), { ssr: false });
 
-export default function Page() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-
-    if (!canvas) {
-      return;
-    }
-
-    const initializeGL = async () => {
-      // IMPORTANT: use dynamic import to avoid loading the entire library at server side
-      const { GLContainer } = await import('@/lib/cores/GLContainer');
-      const { OrthographicCamera } = await import('@/lib/data/components/cameras/OrthographicCamera');
-      const { GLRenderer } = await import('@/lib/rendering/GLRenderer');
-      const { LeonModel } = await import('@/lib/data/models/LeonModel');
-      const { RenderManager} = await import('@/lib/rendering/RenderManager');
-      const { JojoModel } = await import('@/lib/data/models/JojoModel');
-
-      const glContainer = new GLContainer(canvas);
-
-      // const camera = new PerspectiveCamera(
-      //   canvas.width / canvas.height,
-      //   60,
-      //   0.01,
-      //   1000
-      // );
-
-      const camera = new OrthographicCamera(
-        1,
-        -1,
-        -1,
-        1,
-        0.01,
-        1000
-      );
-
-      // const camera = new ObliqueCamera(
-      //   1,
-      //   -1,
-      //   -1,
-      //   1,
-      //   0.01,
-      //   100,
-      //   20
-      // );
-
-      const model = new JojoModel();
-
-      console.log(model.scene.nodes)
-    
-      model.download();
-    };
-
-    initializeGL();
-  }, [canvasRef.current]);
-
-  return (
-    <canvas ref={canvasRef} className="h-screen w-screen"></canvas>
-  );
+export default function Home() {
+    return  <TestPage />   
 }
---
+++ pages/test/testpage.tsx
@@ -0,0 +1,74 @@
+
+import { GLContainer } from "@/lib/cores";
+import { SceneNode } from "@/lib/data/SceneNode";
+import { ObliqueCamera, OrthographicCamera, PerspectiveCamera } from "@/lib/data/components/cameras";
+import { JojoModel } from "@/lib/data/models/JojoModel";
+import { GLRenderer } from "@/lib/rendering/GLRenderer";
+import { useEffect, useRef } from "react";
+
+export default function TestPage() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (!canvas) {
+      return;
+    }
+
+    const initializeGL = async () => {
+      const glContainer = new GLContainer(canvas);
+
+      const orthographicCamera = new OrthographicCamera(
+        1,
+        -1,
+        -1,
+        1,
+        0.01,
+        1000
+      );
+
+      const perspectiveCamera = new PerspectiveCamera(
+          canvas.width / canvas.height,
+          60,
+          0.01,
+          1000
+      );
+
+      const obliqueCamera = new ObliqueCamera(
+          1,
+          -1,
+          -1,
+          1,
+          0.01,
+          100,
+          20
+      );
+
+      const cameraNodes = [
+          new SceneNode({camera: orthographicCamera}),
+          new SceneNode({camera: perspectiveCamera}),
+          new SceneNode({camera: obliqueCamera})
+      ]
+
+      const model = new JojoModel();
+
+      const glRenderer = new GLRenderer(glContainer);
+
+      const scene = model.scene;
+
+      // change camera here
+      scene.addNode(cameraNodes[0]);
+
+      glRenderer.render(scene);
+      
+    //   model.download();
+    };
+
+    initializeGL();
+  }, [canvasRef.current]);
+
+  return (
+    <canvas ref={canvasRef} className="h-screen w-screen"></canvas>
+  );
+}
