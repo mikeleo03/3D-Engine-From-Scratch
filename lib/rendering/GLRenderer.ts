0e0baf741365e028ff136392103de3439c3508c1
Enliven26 <16521443@mahasiswa.itb.ac.id>
2024-05-18 16:15:48 +0700

    Update GLRenderer.ts

--- lib/rendering/GLRenderer.ts
+++ lib/rendering/GLRenderer.ts
@@ -69,6 +69,7 @@ export class GLRenderer {
         }
         
         if (camera.type === CameraTypeString.PERSPECTIVE) {
+            // TODO: jangan modify position asli
             cameraNode.position = new Vector3(0, 0, 700 - (camera.zoom - 1) / 4 * 300)
         }
         let cameraPosition = cameraNode.position;
