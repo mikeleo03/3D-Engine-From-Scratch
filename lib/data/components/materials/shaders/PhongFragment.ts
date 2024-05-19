4ee73349bb2f0be58e755257335de0e09a55e336
mikeleo03 <leonmichael463@gmail.com>
2024-05-19 09:13:34 +0700

    [Refactor] Fragment and Vertex shader, ambient color showed up

--- lib/data/components/materials/shaders/PhongFragment.ts
+++ lib/data/components/materials/shaders/PhongFragment.ts
@@ -13,30 +13,34 @@ varying vec3 v_normal, v_pos;
 
 void main() {
     vec3 normal = normalize(v_normal);
-    vec3 lightDir = normalize(normalize(u_lightPosition) - v_pos);
-    vec3 viewDir = normalize(lightDir + normalize(u_cameraPosition));
+    vec3 lightDir = normalize(u_lightPosition - v_pos);
 
     // Convert colors from 0-255 to 0-1
     vec3 ambient = u_ambientColor.rgb / 255.0;
     vec3 diffuse = u_diffuseColor.rgb / 255.0;
     vec3 specular = u_specularColor.rgb / 255.0;
 
-    // Ambient
-    vec3 ambientComponent = ambient * u_ambientColor.a / 255.0;
+    // Ambient, processed with Ka = 0.1
 
-    // Diffuse
-    float diff = max(dot(lightDir, normal), 0.0);
-    vec3 diffuseComponent = diff * diffuse * u_diffuseColor.a / 255.0;
+    // Diffuse, process labertian
+    float lambertian = max(dot(normal, lightDir), 0.0);
 
-    // Specular
-    // vec3 reflectDir = reflect(-lightDir, normal);
-    float spec = pow(max(dot(normal, viewDir), 0.0), u_shininess);
-    vec3 specularComponent = spec * specular * u_specularColor.a / 255.0;
+    // Specular, Lambert's cosine law
+    float spec = 0.0;
+    if (lambertian > 0.0) {
+        vec3 reflectDir = reflect(-lightDir, normal);       // Reflected light vector
+        vec3 viewDir = normalize(-v_pos);                   // Vector to viewer
+        // Compute the specular term
+        float specAngle = max(dot(reflectDir, viewDir), 0.0);
+        spec = pow(specAngle, u_shininess);
+    }
 
     // Combine all
-    vec3 color = ambientComponent + diffuseComponent + specularComponent;
+    vec3 color = 1.0 * ambient * u_ambientColor.a / 255.0 +
+                1.0 * lambertian * diffuse * u_diffuseColor.a / 255.0 +
+                1.0 * spec * specular * u_specularColor.a / 255.0;
 
     // Apply vertex color
-    gl_FragColor = v_color * vec4(color, 1.0);
+    gl_FragColor = vec4(color, 1.0);
 }
 `;
--- lib/data/components/materials/shaders/PhongVertex.ts
+++ lib/data/components/materials/shaders/PhongVertex.ts
@@ -1,12 +1,12 @@
 export default `
-#define PI 3.1415926535897932384626433832795
-attribute vec4 a_position;
+precision mediump float;
+
+attribute vec3 a_position;
 attribute vec4 a_color;
 attribute vec3 a_normal;
 
 uniform mat4 u_worldMatrix;
 uniform mat4 u_viewMatrix;
-uniform bool u_useVertexColor;
 uniform float u_canvasWidth;
 uniform float u_canvasHeight;
 
@@ -14,12 +14,20 @@ varying vec4 v_color;
 varying vec3 v_normal, v_pos;
 
 void main() {
-    vec4 wPos = u_viewMatrix * u_worldMatrix * a_position;
-    vec2 normalizedPos = vec2(wPos.x / u_canvasWidth + 0.5, wPos.y / u_canvasHeight + 0.5);
-    gl_Position = vec4(normalizedPos * 2.0 - 1.0, wPos.zw);
+    // Transform the vertex position into the clip space
+    vec4 worldPosition = u_worldMatrix * vec4(a_position, 1.0);
+    vec4 viewPosition = u_viewMatrix * worldPosition;
+    vec2 normalizedPos = vec2(viewPosition.x / u_canvasWidth + 0.5, viewPosition.y / u_canvasHeight + 0.5);
 
-    v_pos = gl_Position.xyz / gl_Position.w;
+    gl_Position = vec4(normalizedPos * 2.0 - 1.0, viewPosition.zw);
+
+    // Pass the color attribute to the fragment shader
+    v_color = a_color;
+
+    // Calculate the normal in eye coordinates
     v_normal = mat3(u_worldMatrix) * a_normal;
-    v_color = mix(vec4(1,1,1,1), a_color, float(u_useVertexColor));
+
+    // Pass the position in eye coordinates to the fragment shader    
+    v_pos = vec3(viewPosition);
 }
 `;
