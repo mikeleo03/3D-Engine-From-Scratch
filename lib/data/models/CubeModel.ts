d91c50600ec02cfd95c259892fc61126fd908a6a
mikeleo03 <leonmichael463@gmail.com>
2024-05-19 19:20:07 +0700

    [Feat] Add another cube on the model

--- lib/data/models/CubeModel.ts
+++ lib/data/models/CubeModel.ts
@@ -14,20 +14,33 @@ export class CubeModel extends Model {
 
     private getCube(): SceneNode {
         const meshFactory = new MeshFactory();
-        const cubeMaterial = new PhongMaterial({ name: "cube", ambientColor: new Color(52, 25, 0), diffuseColor: new Color(204, 102, 0), specularColor: new Color(255, 255, 255), shininess: 80, lightPosition: new Vector3(120, -120, 120)});
-        const cubeMesh = meshFactory.cuboid(100, 100, 100, [cubeMaterial]);
+        const cubeMaterial = new PhongMaterial({ name: "cube", ambientColor: new Color(1 * 255, 0.5 * 255, 0.31 * 255), diffuseColor: new Color(1 * 255, 0.5 * 255, 0.31 * 255), specularColor: new Color(0.5 * 255, 0.5 * 255, 0.5 * 255), shininess: 32, lightPosition: new Vector3(20, 70, 30)});
+        const cubeMesh = meshFactory.cuboid(50, 50, 50, [cubeMaterial]);
         return new SceneNode({name: 'Cube', mesh: cubeMesh});
+    }
+
+    private getCubeLight(): SceneNode {
+        const meshFactory = new MeshFactory();
+        const cubeLightMaterial = new PhongMaterial({ name: "cubeLight", ambientColor: new Color(0.2 * 255, 0.2 * 255, 0.2 * 255), diffuseColor: new Color(0.5 * 255, 0.5 * 255, 0.5 * 255), specularColor: new Color(1.0 * 255, 1.0 * 255, 1.0 * 255), shininess: 32, lightPosition: new Vector3(-10, 90, 0)});
+        const cubeLightMesh = meshFactory.cuboid(20, 20, 20, [cubeLightMaterial]);
+        return new SceneNode({name: 'Cube Light', mesh: cubeLightMesh});
     }
 
     protected override getScene() {
         const nodes: SceneNode[] = [];
 
         const cube = this.getCube();
+        const cubeLight = this.getCubeLight();
+
+        cubeLight.translate(new Vector3(20, 70, 30));
+        // cube.translate(new Vector3(200, 20, 20));
+
         const parent = new SceneNode({name: 'Cube Model'});
+        parent.add(cubeLight);
         parent.add(cube);
 
         parent.translate(new Vector3(0, 0, 0));
-        parent.rotateByDegrees(new Vector3(30, 30, 0));
+        parent.rotateByDegrees(new Vector3(30, -30, 0));
 
         nodes.push(parent);
         return new Scene(nodes);
