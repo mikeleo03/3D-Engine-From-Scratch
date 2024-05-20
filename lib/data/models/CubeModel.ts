09f80b9d76d2f8cbe7f5374c63cba7f60df774cc
mikeleo03 <leonmichael463@gmail.com>
2024-05-20 16:01:47 +0700

    [Feat] Recolor cubemodel

--- lib/data/models/CubeModel.ts
+++ lib/data/models/CubeModel.ts
@@ -14,29 +14,19 @@ export class CubeModel extends Model {
 
     private getCube(): SceneNode {
         const meshFactory = new MeshFactory();
-        const cubeMaterial = new PhongMaterial({ name: "cube", ambientColor: new Color(1 * 255, 0.5 * 255, 0.31 * 255), diffuseColor: new Color(1 * 255, 0.5 * 255, 0.31 * 255), specularColor: new Color(0.5 * 255, 0.5 * 255, 0.5 * 255), shininess: 32, lightPosition: new Vector3(20, 70, 30)});
-        const cubeMesh = meshFactory.cuboid(50, 50, 50, [cubeMaterial]);
+        const cubeMaterial = new PhongMaterial({ name: "cube", ambientColor: new Color(1 * 255, 0.5 * 255, 0.31 * 255), diffuseColor: new Color(1 * 255, 0.5 * 255, 0.31 * 255), specularColor: new Color(0.5 * 255, 0.5 * 255, 0.5 * 255), shininess: 32});
+        const cubeMesh = meshFactory.cuboid(80, 80, 80, [cubeMaterial]);
         return new SceneNode({name: 'Cube', mesh: cubeMesh});
-    }
-
-    private getCubeLight(): SceneNode {
-        const meshFactory = new MeshFactory();
-        const cubeLightMaterial = new PhongMaterial({ name: "cubeLight", ambientColor: new Color(0.2 * 255, 0.2 * 255, 0.2 * 255), diffuseColor: new Color(0.5 * 255, 0.5 * 255, 0.5 * 255), specularColor: new Color(1.0 * 255, 1.0 * 255, 1.0 * 255), shininess: 32, lightPosition: new Vector3(-10, 90, 0)});
-        const cubeLightMesh = meshFactory.cuboid(20, 20, 20, [cubeLightMaterial]);
-        return new SceneNode({name: 'Cube Light', mesh: cubeLightMesh});
     }
 
     protected override getScene() {
         const nodes: SceneNode[] = [];
 
         const cube = this.getCube();
-        const cubeLight = this.getCubeLight();
 
-        cubeLight.translate(new Vector3(20, 70, 30));
-        // cube.translate(new Vector3(200, 20, 20));
+        cube.translate(new Vector3(10, 10, 10));
 
         const parent = new SceneNode({name: 'Cube Model'});
-        parent.add(cubeLight);
         parent.add(cube);
 
         parent.translate(new Vector3(0, 0, 0));
