e5ce31a782d331d719c4941cfb8d772fac39b3ed
Marthenn <89301265+Marthenn@users.noreply.github.com>
2024-05-24 00:15:18 +0700

    fix (model): animation head rotation

--- lib/data/models/MarthenModel.ts
+++ lib/data/models/MarthenModel.ts
@@ -2,7 +2,7 @@ import { Color } from "@/lib/cores"
 import { Scene } from "../Scene"
 import { SceneNode } from "../SceneNode"
 import { AnimationClip } from "../components/animations"
-import { BasicMaterial } from "../components/materials"
+import {BasicMaterial, PhongMaterial} from "../components/materials"
 import { MeshFactory } from "../components/mesh/MeshFactory"
 import { Model } from "./Model"
 import { Quaternion, Vector3 } from "../math"
@@ -17,35 +17,76 @@ export class MarthenModel extends Model {
   private getHead() {
     const meshFactory = new MeshFactory();
     const headMaterial = new BasicMaterial(new Color(187, 226, 236), { name: "head" });
-    const headMesh = meshFactory.cuboid(50, 50, 50, {basicMaterial: headMaterial}, { offset: [0, 12.5, 0] });
+    const phongHeadMaterial = new PhongMaterial({
+      name: "head-phong",
+      ambientColor: new Color(187, 226, 236),
+      diffuseColor: new Color(187, 226, 236),
+      specularColor: new Color(255, 255, 255),
+      shininess: 20
+    })
+
+    const headMesh = meshFactory.cuboid(50, 50, 50,
+      {basicMaterial: headMaterial, phongMaterial: phongHeadMaterial}, { offset: [0, 25, 0] });
     return new SceneNode({mesh: headMesh});
   }
 
   private getBody() {
     const meshFactory = new MeshFactory();
     const bodyMaterial = new BasicMaterial(new Color(13, 146, 118), { name: "body" });
-    const bodyMesh = meshFactory.cuboid(100, 70, 100, {basicMaterial: bodyMaterial});
+    const phongBodyMaterial = new PhongMaterial({
+      name: "body-phong",
+      ambientColor: new Color(13, 146, 118),
+      diffuseColor: new Color(13, 146, 118),
+      specularColor: new Color(255, 255, 255),
+      shininess: 80
+    })
+    const bodyMesh = meshFactory.cuboid(100, 70, 100,
+      {basicMaterial: bodyMaterial, phongMaterial: phongBodyMaterial});
     return new SceneNode({mesh: bodyMesh});
   }
 
   private getLeg() {
     const meshFactory = new MeshFactory();
     const legMaterial = new BasicMaterial(new Color(64, 162, 227), { name: "leg" });
-    const legMesh = meshFactory.cuboid(23, 40, 25, {basicMaterial: legMaterial}, { offset: [0, -25, 0] });
+    const phongLegMaterial = new PhongMaterial({
+      name: "leg-phong",
+      ambientColor: new Color(64, 162, 227),
+      diffuseColor: new Color(64, 162, 227),
+      specularColor: new Color(255, 255, 255),
+      shininess: 40
+    })
+    const legMesh = meshFactory.cuboid(23, 40, 25,
+      {basicMaterial: legMaterial, phongMaterial: phongLegMaterial}, { offset: [0, -20, 0] });
     return new SceneNode({mesh: legMesh});
   }
 
   private getEye() {
     const meshFactory = new MeshFactory();
     const eyeMaterial = new BasicMaterial(new Color(0, 0, 0), { name: "eye" });
-    const eyeMesh = meshFactory.cuboid(10, 10, 10, {basicMaterial: eyeMaterial});
+    const phongEyeMaterial = new PhongMaterial({
+      name: "eye-phong",
+      ambientColor: new Color(0, 0, 0),
+      diffuseColor: new Color(0, 0, 0),
+      specularColor: new Color(255, 255, 255),
+      shininess: 80
+    })
+    const eyeMesh = meshFactory.cuboid(10, 10, 10,
+      {basicMaterial: eyeMaterial});
     return new SceneNode({mesh: eyeMesh});
   }
 
   private getMouth() {
     const meshFactory = new MeshFactory();
     const mouthMaterial = new BasicMaterial(new Color(0, 0, 0), { name: "mouth" });
-    const mouthMesh = meshFactory.cuboid(20, 10, 10, {basicMaterial: mouthMaterial});
+    const phongMouthMaterial = new PhongMaterial({
+      name: "mouth-phong",
+      ambientColor: new Color(0, 0, 0),
+      diffuseColor: new Color(0, 0, 0),
+      specularColor: new Color(255, 255, 255),
+      shininess: 0
+    })
+    const mouthMesh = meshFactory.cuboid(20, 10, 10,
+      {basicMaterial: mouthMaterial, phongMaterial: phongMouthMaterial});
     return new SceneNode({mesh: mouthMesh});
   }
 
@@ -63,28 +104,40 @@ export class MarthenModel extends Model {
     const rightEye = this.getEye();
     const mouth = this.getMouth();
 
+    head.name = "Head";
+    body.name = "Body";
+    leftFrontLeg.name = "Left Front Leg";
+    rightFrontLeg.name = "Right Front Leg";
+    leftBackLeg.name = "Left Back Leg";
+    rightBackLeg.name = "Right Back Leg";
+    leftEye.name = "Left Eye";
+    rightEye.name = "Right Eye";
+    mouth.name = "Mouth";
+
     // translation to make the position of the limbs and other parts of the body
     body.translate(new Vector3(0, 10, -80));
-    leftFrontLeg.translate(new Vector3(-35, -20, -120));
-    rightFrontLeg.translate(new Vector3(35, -20, -120));
-    leftBackLeg.translate(new Vector3(-35, -20, -50));
-    rightBackLeg.translate(new Vector3(35, -20, -50));
+    leftFrontLeg.translate(new Vector3(-35, -20, 37));
+    rightFrontLeg.translate(new Vector3(35, -20, 37));
+    leftBackLeg.translate(new Vector3(-35, -20, -37));
+    rightBackLeg.translate(new Vector3(35, -20, -37));
 
-    leftEye.translate(new Vector3(-15, 20, 25));
-    rightEye.translate(new Vector3(15, 20, 25));
-    mouth.translate(new Vector3(0, 0, 25));
+    head.translate(new Vector3(0, 0, 0));
+    leftEye.translate(new Vector3(-15, 30, 25));
+    rightEye.translate(new Vector3(15, 30, 25));
+    mouth.translate(new Vector3(0, 10, 25));
 
     head.add(leftEye);
     head.add(rightEye);
     head.add(mouth);
 
-    const parent = new SceneNode();
+    body.add(leftFrontLeg);
+    body.add(rightFrontLeg);
+    body.add(leftBackLeg);
+    body.add(rightBackLeg);
+
+    const parent = new SceneNode({name: "Turtle"});
     parent.add(head);
     parent.add(body);
-    parent.add(leftFrontLeg);
-    parent.add(rightFrontLeg);
-    parent.add(leftBackLeg);
-    parent.add(rightBackLeg);
 
     parent.translate(new Vector3(0, 0, -100));
     parent.rotateByDegrees(new Vector3(30, 30, 0));
@@ -96,287 +149,26 @@ export class MarthenModel extends Model {
 
   override getAnimations(): AnimationClip[] {
     const headRef = this.scene.nodes[0].children[0];
-    return [
-      {
-        name: "idle",
-        frames: [
+    const frames = [];
+
+    for (let i = 0; i <= 360; i += 10) {
+      frames.push({
+        nodeKeyframePairs: [
           {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  rotation: [0, 0, 0],
-                  translation: [0, 0, 0]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 15],
-                  rotation: [0, 0, 15]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 30],
-                  rotation: [0, 0, 30]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 30]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 60]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 75]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 90]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 105]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 120]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 135]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 150]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 165]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 180]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 195]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 210]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 225]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 240]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 255]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 270]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 285]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 300]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 315]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 330]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 345]
-                }
-              }
-            ]
-          },
-          {
-            nodeKeyframePairs: [
-              {
-                node: headRef,
-                keyframe: {
-                  translation: [0, 0, 45],
-                  rotation: [0, 0, 360]
-                }
-              }
-            ]
+            node: headRef,
+            keyframe: {
+              rotation: [0, 0, i]
+            }
           }
         ]
+      })
+    }
+
+    return [
+      {
+        name: "head-rotation",
+        frames: frames
       }
-    ];
+    ]
   }
 }
