34adbe35d13153639ba68a19558f03e40f7632fb
Enliven26 <16521443@mahasiswa.itb.ac.id>
2024-05-26 19:44:02 +0700

    refactor: displacement mapping

--- lib/data/models/CubeModel.ts
+++ lib/data/models/CubeModel.ts
@@ -178,14 +178,14 @@ export class CubeModel extends Model {
         ]);
 
         const mapping2 = [
-            0, 0,
-            0, 1,
-            1, 0,
+            1, 1,
+            1, 1,
+            1, 1,
             1, 1,
             0, 0,
-            0, 1,
-            1, 0,
-            1, 1
+            0, 0,
+            0, 0,
+            0, 0
         ]
 
         const data3 = new Uint8Array([
@@ -224,7 +224,6 @@ export class CubeModel extends Model {
             diffuseColor: new Color(204, 102, 0), 
             specularColor: new Color(255, 255, 255), 
             shininess: 60,
-            diffuseMap: diffuseDatas[0],
             diffuseMaps: diffuseDatas,
             normalMaps: [],
             displacementMaps: displacementDatas,
