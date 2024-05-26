ac6853bc9d45054cc29f133977de0295d7b8023e
Marthenn <89301265+Marthenn@users.noreply.github.com>
2024-05-26 16:20:10 +0700

    feat (texture): add flag for checking usage of normal map

--- lib/rendering/GLRenderer.ts
+++ lib/rendering/GLRenderer.ts
@@ -91,6 +91,7 @@ export class GLRenderer {
                     textureUniforms["displacementScale"] = material.displacementMap?.scale;
                     textureUniforms["displacementBias"] = material.displacementMap?.bias;
                     textureUniforms["normalMap"] = material.normalMap?.texture;
+                    textureUniforms["hasNormalMap"] = material.normalMap ? 1.0 : 0.0;
                     textureUniforms["diffuseMap"] = material.diffuseMap?.texture;
                     textureUniforms["specularMap"] = material.specularMap?.texture;
                 }
