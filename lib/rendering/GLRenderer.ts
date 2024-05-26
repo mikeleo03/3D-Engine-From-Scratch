4c824f8f132b1467b4392d275ee464b300d95833
mikeleo03 <leonmichael463@gmail.com>
2024-05-26 15:39:19 +0700

    [Fix[ Logic for texture detection

--- lib/rendering/GLRenderer.ts
+++ lib/rendering/GLRenderer.ts
@@ -92,9 +92,9 @@ export class GLRenderer {
                     textureUniforms["displacementBias"] = material.displacementMap?.bias;
                     textureUniforms["normalMap"] = material.normalMap?.texture;
                     textureUniforms["diffuseMap"] = material.diffuseMap?.texture;
-                    textureUniforms["hasDiffuseMap"] = material.diffuseMap != null ? 1.0 : 0.0;
+                    textureUniforms["hasDiffuseMap"] = material.diffuseMaps.length > 0 && material.diffuseMap != undefined ? 1.0 : 0.0;
                     textureUniforms["specularMap"] = material.specularMap?.texture;
-                    textureUniforms["hasSpecularMap"] = material.specularMap != null ? 1.0 : 0.0;
+                    textureUniforms["hasSpecularMap"] = material.specularMaps.length > 0 && material.specularMap != undefined ? 1.0 : 0.0;
                 }
     
                 const lightUniforms: { [key: string]: UniformSingleDataType | undefined } = {};
