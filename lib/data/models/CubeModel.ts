56f3eb3f0a7ef136fd4b26b32643ec4a12622bd6
mikeleo03 <leonmichael463@gmail.com>
2024-05-26 16:58:20 +0700

    [Feat] Set default params

--- lib/data/models/CubeModel.ts
+++ lib/data/models/CubeModel.ts
@@ -194,9 +194,11 @@ export class CubeModel extends Model {
             diffuseColor: new Color(204, 102, 0), 
             specularColor: new Color(255, 255, 255), 
             shininess: 60,
+            diffuseMap: diffuseDatas[0],
             diffuseMaps: diffuseDatas,
             normalMaps: [],
             displacementMaps: displacementDatas,
+            specularMap: specularDatas[0],
             specularMaps: specularDatas
         });
 
